797d4fb2ead5f610ebb9e86763cd715ccf23228e
Jaeseo Bae <jae912@uab.edu>
2022-08-14 21:07:46 -0500

    Added smooth scrolling and finished content-wise for the Hero Banner

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
+// When we turn on our lightbulb
 let checkbox = document.querySelector('#toggle');
 let body = document.querySelector('body');
 let banner = document.querySelector('.banner');
 let circle2 = document.querySelector('.switch-circle');
 let circle1 = document.querySelector('.circle');
+let showMore = document.querySelector('.hidden');
 checkbox.onclick = function() {
     body.classList.toggle('on');
     banner.classList.toggle('on');
     circle2.classList.toggle('on');
     circle1.classList.toggle('on');
+    showMore.classList.toggle('on');
 }
+
+// Smooth Scrolling for anchor tags
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    }); 
+});
